173c130d45837f0f01cbd0c396241cf4829e62f2
yorunaka <vincent.fernando12345@gmail.com>
2024-07-03 13:34:09 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def clear_input_field():
+    st.session_state.user_question = st.session_state.user_input
+    st.session_state.user_input = ""
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
+def main():
+    st.title("SakuyaAI")
+    st.header("Your Personal Assistant")
+    chat_container = st.container()
+
+    if "send_input" not in st.session_state:
+        st.session_state.send_input = False
+        st.session_state.user_question = ""
+
+    user_input = chat_container.text_input("Type your message here", "", key="user_input", on_change=set_send_input)
+
+    send_button = chat_container.button("Send", key="send_button")
+
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
+            llm_response = "This is a response from SakuyaAI"
+
+            with chat_container:
+                st.chat_message("user").write(st.session_state.user_question)
+                st.chat_message("SakuyaAI").write("This is a response from SakuyaAI")
+
+if __name__ == "__main__":
+    main()
+
+
